e1b1573fda78a63551d3bbd1697e4c4ea3a41bea
Rachel Nguyen <thaontt1408@gmail.com>
2020-11-27 14:44:01 -0700

    Update Project2

--- project2.js
+++ project2.js
@@ -1,7 +1,52 @@
-function plusFunction() {
-    var No1 = parseFloat(document.getElementById("No1").value);
-    var No2 = parseFloat(document.getElementById("No2").value);
-    var total = No1 + No2;
-    document.getElementById("result").innerHTML = total;
+var No1;
+var No2;
 
+function getValue() {
+//obtain user input
+ No1 = parseFloat(document.getElementById("No1").value);
+ No2 = parseFloat(document.getElementById("No2").value);
 }
+function addFunction() {
+    getValue();
+
+    //calculation
+    var total = No1 + No2;
+
+    if(!isNaN(total)) {
+        document.getElementById("result").innerHTML = total;
+        console.log(total);
+
+    }
+};
+function minFunction() {
+    getValue();
+    //calculation
+    var min = No1 - No2;
+
+    if(!isNaN(min)) {
+        document.getElementById("result").innerHTML = min;
+        console.log(min);
+
+    }
+};
+
+// function mulFunction() {
+//     //calculation
+//     var mul = No1 * No2;
+
+//     if(!isNaN(mul)) {
+//         document.getElementById("result").innerHTML = mul;
+//         console.log(mul);
+
+//     }
+// };
+// function divFunction() {
+//     //calculation
+//     var div = No1 / No2;
+
+//     if(!isNaN(div)) {
+//         document.getElementById("result").innerHTML = div;
+//         console.log(div);
+
+//     }
+// };

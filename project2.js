241d0e88e22385502617aeb11088b87950e99eea
Rachel Nguyen <thaontt1408@gmail.com>
2020-11-27 14:49:02 -0700

    finalize project 2

--- project2.js
+++ project2.js
@@ -2,9 +2,9 @@ var No1;
 var No2;
 
 function getValue() {
-//obtain user input
- No1 = parseFloat(document.getElementById("No1").value);
- No2 = parseFloat(document.getElementById("No2").value);
+    //obtain user input
+    No1 = parseFloat(document.getElementById("No1").value);
+    No2 = parseFloat(document.getElementById("No2").value);
 }
 function addFunction() {
     getValue();
@@ -12,7 +12,7 @@ function addFunction() {
     //calculation
     var total = No1 + No2;
 
-    if(!isNaN(total)) {
+    if (!isNaN(total)) {
         document.getElementById("result").innerHTML = total;
         console.log(total);
 
@@ -23,30 +23,31 @@ function minFunction() {
     //calculation
     var min = No1 - No2;
 
-    if(!isNaN(min)) {
+    if (!isNaN(min)) {
         document.getElementById("result").innerHTML = min;
         console.log(min);
 
     }
 };
 
-// function mulFunction() {
-//     //calculation
-//     var mul = No1 * No2;
+function mulFunction() {
+    getValue();
+    //calculation
+    var mul = No1 * No2;
 
-//     if(!isNaN(mul)) {
-//         document.getElementById("result").innerHTML = mul;
-//         console.log(mul);
+    if (!isNaN(mul)) {
+        document.getElementById("result").innerHTML = mul;
+        console.log(mul);
+    }
+};
 
-//     }
-// };
-// function divFunction() {
-//     //calculation
-//     var div = No1 / No2;
+function divFunction() {
+    getValue();
+    //calculation
+    var div = No1 / No2;
 
-//     if(!isNaN(div)) {
-//         document.getElementById("result").innerHTML = div;
-//         console.log(div);
-
-//     }
-// };
+    if (!isNaN(div)) {
+        document.getElementById("result").innerHTML = div;
+        console.log(div);
+    }
+};

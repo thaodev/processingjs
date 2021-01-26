a1937fb1e7073ede78aac99cb3cb206305d40d85
Rachel Nguyen <thaontt1408@gmail.com>
2021-01-26 07:18:22 -0700

    Separate js file for project4

---
+++ Project4/project4.js
@@ -0,0 +1,54 @@
+$("button").hover(function () {
+    $(this).addClass("color");
+},
+    function () {
+        $(this).removeClass("color");
+    });
+
+
+
+var slide = 0;
+
+function showSlides() {
+    $(".image-area").hide();
+    $(".image-area:eq(" + slide + ")").show();
+}
+showSlides();
+
+//For buttons 1, 2, 3, and 4
+function changeSlide(index) {
+    slide = index - 1;
+    for (let i = 0; i < $(".button").length; i++) {
+        $(".button:eq(" + i + ")").css("background-color", "#03e8fc");
+    }
+    $(".button:eq(" + slide + ")").css("background-color", "gray");
+    showSlides();
+}
+
+//For Next Button
+$("#Next").on("click", function () {
+    if (slide == $(".image-area").length - 1) {
+        slide = 0;
+    } else {
+        slide++;
+    }
+    for (let i = 0; i < $(".button").length; i++) {
+        $(".button:eq(" + i + ")").css("background-color", "#03e8fc");
+    }
+    $(".button:eq(" + slide + ")").css("background-color", "gray");
+    showSlides();
+})
+
+//For Previous button
+$("#Prev").on("click", function () {
+    if (slide == 0) {
+        slide = $(".image-area").length - 1;
+    } else {
+        slide--;
+    }
+    for (let i = 0; i < $(".button").length; i++) {
+        $(".button:eq(" + i + ")").css("background-color", "#03e8fc");
+    }
+    $(".button:eq(" + slide + ")").css("background-color", "gray");
+    showSlides();
+})
